67b9439b18951a0a8341c7988e58446db4b4df38
Eloise-Slominski <eloiseslominski@seattleacademy.org>
2025-01-13 17:17:37 +0000

    code art (edited)

--- Projects/code_art.py
+++ Projects/code_art.py
@@ -4,13 +4,6 @@ t = turtle.Turtle()
 
 t.goto(-15, 15)
 
-#blue and pink star
-colors=["pink","blue"]
-for i in range(20):
-    t.color(colors[i%2])
-    t.forward(15+14)
-    t.left(144+1)
-
 t.pendown()
 t.goto(0,0)
 
@@ -23,6 +16,51 @@ for i in range(60):
 #orange spiral
 t.color("orange")
 for i in range (60):
+    t.forward(20+i)
+    t.left(45)
+
+    t.penup()
+    t.goto(-100,70)
+    t.pendown()
+
+#blue star thing
+    t.color("blue")
+for i in range (60):
+    t.forward(20+i)
+    t.left(45)
+
+#purple spiral
+    t.color("purple")
+for i in range (30):
+    t.forward(20+i)
+    t.left(45)
+
+t.penup()
+t.goto(-90,-120)
+t.pendown()
+#yellow spiral
+t.color("yellow")
+for i in range (60):
+    t.forward(20+i)
+    t.left(45)
+
+
+t.penup()
+t.goto(130,0)
+t.pendown()
+
+#green spiral
+t.color("green")
+for i in range (60):
+    t.forward(20+i)
+    t.left(45)
+
+t.penup()
+t.goto(170,70)
+t.pendown()
+#blue spiral
+t.color("blue")
+for i in range(60):
     t.forward(20+i)
     t.left(45)
 
